7ea4f8780c6193bb708834e24029a77102b780bd
M.G. Ting <conan199306@sina.com>
2019-04-12 17:28:24 +0800

    feat: Fetch all top 250 movies with delay

--- douban_top250_excel.py
+++ douban_top250_excel.py
@@ -3,14 +3,47 @@
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+import time
 
 # 1 获取内容
-url = 'https://movie.douban.com/top250'
+baseuUrl = 'https://movie.douban.com/top250'
+
+def fetch(start):
+    # 25 per page
+    url = baseuUrl + '?start=' + str(start)
+    end = start + 25
+    print('Fetch ', start, '-', end)
+    html = requests.get(url)
+    document = BeautifulSoup(html.content, features="html.parser")
+
+
+    # 2 解析内容
+    movies25 = document.select('ol.grid_view > li')
+
+    for movie in movies25:
+
+        title = movie.find('span', class_='title').get_text(strip=True) # 使用string 得到的类型是节点，不是python的string
+        star = movie.find('span', class_='rating_num').get_text(strip=True)
+        try:
+            quote = movie.find('span', class_='inq').get_text(strip=True)
+        except AttributeError as e:
+            print('No Quote:', title)
+            quote = ''
+
+    # 3 保存
+        # print(title, star, quote)
+        cell = [title, star, quote]
+        ws.append(cell)
+
+    start = end
+    if start == 250:
+        return;
+    else:
+        #threading.Timer(3, fetch, (start,)).start()    # 会导致主线程直接运行完成，timer必须使用iterable传参数
+        time.sleep(1)
+        fetch(start)
 
 print('Begin fetching data...')
-
-html = requests.get(url)
-document = BeautifulSoup(html.content, features="html.parser")
 
 # 1.1 新建excel
 wb = Workbook()
@@ -19,19 +52,7 @@ ws = wb.active
 # 1.3 初始化表头
 ws.append(['影片名', '评分', '评语'])
 
-# 2 解析内容
-movies50 = document.select('ol.grid_view > li')
-
-for movie in movies50:
-
-    title = movie.find('span', class_='title').get_text(strip=True) # 使用string 得到的类型是节点，不是python的string
-    star = movie.find('span', class_='rating_num').get_text(strip=True)
-    quote = movie.find('span', class_='inq').get_text(strip=True)
-
-# 3 保存
-    # print(title, star, quote)
-    cell = [title, star, quote]
-    ws.append(cell)
+fetch(0)
 
 # Data can be assigned directly to cells
 # ws['A1'] = 42
